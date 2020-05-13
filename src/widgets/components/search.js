5153e3364ed7a38df8fd594b02c113a3735dcb57
Marco Madera <marcoseguro2@gmail.com>
2020-05-13 09:08:27 -0700

    prompt added to the search box of videos

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './search.css';
+import { Prompt } from 'react-router'
 // function Search(props) {
 //   return (
 //     <form action=""></form>
@@ -11,6 +12,10 @@ const Search = (props) => (
     className="Search"
     onSubmit={props.handleSubmit}
   >
+    <Prompt 
+      when={props.prompt}
+      message="Seguro que quieres dejar la página?"
+    />
     <input
       ref={props.setRef}
       type="text"
